a21c83db46b14e49231d0aa48c989b2a4d6d6678
shubhammate013 <mateshubham7478@gmail.com>
2024-07-22 22:58:35 +0530

    Successfully added

---
+++ public/js/index.js
@@ -0,0 +1,6 @@
+// scroll reveal
+ScrollReveal({ reset: true });
+
+// index
+ScrollReveal().reveal('.imgs', { delay: 500, distance: '50px', origin: 'right' });
+ScrollReveal().reveal('.jumborton-content', { delay: 500, distance: '50px', origin: 'left' });
